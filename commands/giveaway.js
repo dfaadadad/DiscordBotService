c53070747f25bd4d679fc2623b991ad67083ac43
dfaadadad <56511136+dfaadadad@users.noreply.github.com>
2019-11-23 13:29:49 +0100

    init
    init

---
+++ commands/giveaway.js
@@ -0,0 +1,117 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) =>{
+
+
+    var item = "";
+    var time;
+    var winnerCount;
+
+    if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send ("Sorry, you can't start an giveaway!");
+
+     //  !giveaway aantaal tijd item
+
+     winnerCount = args[0]; 
+     time = args[1];
+     item = args.splice(2,args.length).join(" "); 
+
+     message.delete();
+
+     var date = new Date().getTime();
+     var dateTime = new Date(date + (time * 1000)); 
+
+     var embed = new discord.RichEmbed()
+     .setTitle("🎉 **GIVEAWAY!** 🎉")
+     .setFooter(`Ends on: ${dateTime}`)
+     .setColor("#44ffa8")
+     .setDescription(item); 
+
+     var embedSend = await message.channel.send(embed);
+     embedSend.react("🎉"); 
+
+     setTimeout(function()  {
+      
+        var random = 0; 
+        var winners = [];
+        var inList = false; 
+
+        var peopleReacted = embedSend.reactions.get("🎉").users.array();
+
+        for (let i = 0; i < peopleReacted.length; i++) {
+           
+            if(peopleReacted[i].id == bot.user.id) {
+
+
+               peopleReacted.splice(i,1);
+               continue; 
+
+
+
+            }
+            
+        }
+
+
+        if(peopleReacted == 0){
+
+            return message.channel.send("Nobody wins, so the bot wins!");
+
+        }
+        if(peopleReacted < winnerCount){
+
+            return message.channel.send("Too few users participated!");
+            
+        }
+
+        for (let i  = 0; i  < winnerCount; i ++) {
+            
+            inList = false; 
+
+           random = Math.floor(Math.random() * peopleReacted.length);
+
+           for (let y = 0; y < winners.length; y++) {
+               
+              if(winners[y] == peopleReacted[random]){
+                  inList = true; 
+                  i--; 
+                  break; 
+          
+              }
+          
+            }
+
+
+            if(!inList){
+
+             winners.push(peopleReacted[random]); 
+
+
+
+            }
+                    
+
+            for (let i = 0; i < winners.length; i++) {
+                
+                message.channel.send("Congrats " + winners[i] + `! You win **${item}**!`)
+                
+            }
+          
+
+        }
+
+
+     }, time * 1000); 
+
+     
+
+
+
+
+
+
+}
+
+module.exports.help = {
+  name: "giveaway"
+
+}
---
+++ index.js
@@ -0,0 +1,137 @@
+const discord = require("discord.js");
+const botConfig = require("./botConfig.json");
+
+
+const fs = require("fs");
+ 
+const bot = new discord.Client();
+bot.commands = new discord.Collection(); 
+
+fs.readdir("./commands/", (err, files) => {
+
+ if(err) console.log(err); 
+
+
+ var jsFiles = files.filter(f => f.split(".").pop() === "js"); 
+
+  if (jsFiles.length <= 0) {
+     console.log("Cannot find files");
+     return; 
+
+  }
+
+
+   jsFiles.forEach((f,i) => {
+
+    var fileGet = require(`./commands/${f}`);
+    console.log(`The file ${f} has loaded `);
+
+    bot.commands.set(fileGet.help.name, fileGet); 
+
+
+
+
+   })
+
+
+
+
+});
+
+
+bot.on("ready", async () => {
+
+    console.log(`${bot.user.username} is now online!`)
+
+    bot.user.setActivity("me getting developed..", { type: "WATCHING" });
+
+});
+
+
+bot.on("guildMemberAdd", member => {
+
+    var role = member.guild.roles.find("name", "§ Member");
+
+    if (!role) return;
+
+    member.addRole(role); 
+
+    const channel = member.guild.channels.find("name", "hi-and-bye");
+
+    if (!channel) return;
+
+    channel.send(`Welcome in our server ${member}! \n **Type !help for more information!**`);
+
+
+
+
+
+});
+
+
+var swearWords = ["fuck", "shit", "omg", "nigger", "nigga", "nibba", "cancer", "dick", "pussy", "dumbass", "fy", "ligma", "gay"];
+
+
+bot.on("message", async message => {
+
+    //Als dit stuurt, stuur dan dat
+
+    if (message.author.bot) return;
+
+    if (message.channel.type === "dm") return;
+
+    var prefix = botConfig.prefix;
+
+    var messageArray = message.content.split(" ");
+
+    var command = messageArray[0];
+
+    var arguments = messageArray.slice(1);
+
+    var commands = bot.commands.get(command.slice(prefix.length));
+
+    if(commands) commands.run(bot,message, arguments); 
+
+    var msg = message.content.toLowerCase(); 
+
+    for(var i = 0; i < swearWords.length; i++) {
+
+    if(msg.includes(swearWords[i])) {
+
+     message.delete();
+
+     return message.channel.send("Please do not swear!").then(msg => msg.delete(3000)); 
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+  
+
+
+
+   
+
+    
+
+
+ 
+
+
+
+
+
+});
+
+
+bot.login(process.env.token);
